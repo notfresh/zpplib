8e693947c7a33e67bba540e9e300d5aa8e1d5493
notfreshmbp <notfresh@foxmail.com>
2024-04-05 07:39:58 +0800

    refactor:调整代码结构

---
+++ Makefile
@@ -0,0 +1,3 @@
+# 注意，make 安装依赖的功能暂时不可用。使用本项目请使用先试用 bazel。
+hello:
+	echo "hello world"
---
+++ release-zip.sh
@@ -0,0 +1,4 @@
+zip -r zpplib-release-v0.1.zip .  \
+ -x "bazel-zpplib/*"  -x "bazel-bin/*" -x "bazel-out/*" -x ".git/*" \
+ -x "bazel-testlogs/*"
+
